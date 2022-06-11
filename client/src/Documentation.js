5294cadfefab25e451efcbde8e67c9922f598935
Gabriela <ukka218@gmail.com>
2022-06-11 20:36:35 +0200

    first commit

---
+++ client/src/Documentation.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import arch from './arch.png';
+
+export default () => {
+  return (
+    <div>
+      <Link to="/">Go back home</Link>
+      <br />
+      Usługa jest zmodyfikowaną wersją przykładu z lab 11. Zmodyfikowane zostały:
+      <br />
+      <ul>
+        <li>funkcja fib w części worker zmieniona na funkcję gs obliczającą wartości dla ciągu geometrycznego</li>
+        <li>w części client fib.js zmienionw na gs.js i zmieniony tekst </li>
+        <li>zmieniony tekst strony głównej (App.js)</li>
+        <li>zaaktualizowana strona Documentation</li>
+        <li></li>
+      </ul>
+    </div>
+  );
+};
